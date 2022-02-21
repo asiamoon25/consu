64c8ffcb8d8d6cd31cdf0484c1ef4af509a1fd0a
LENOVO <wjddustkd45@gmail.com>
2022-02-21 10:22:41 +0900

    Initial commit

---
+++ src/main/java/com/study/consu/ConsuApplication.java
@@ -0,0 +1,13 @@
+package com.study.consu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/consu/ConsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.consu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
